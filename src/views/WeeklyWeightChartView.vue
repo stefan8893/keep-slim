8e8aff8f753502922e5ecd5487b54747d7192570
Stefan Jeitler <stefan.jeitler@outlook.com>
2025-07-10 20:24:59 +0200

    feat: first chart scaffolded

--- src/views/WeeklyWeightChartView.vue
+++ src/views/WeeklyWeightChartView.vue
@@ -1,18 +1,39 @@
 <script setup lang="ts">
+import { themingControlKey } from '@/injection.types';
+import type { ThemingControl } from '@/plugins/theming.plugin';
 import Highcharts from 'highcharts';
-import { onMounted, ref } from 'vue';
+import 'highcharts/modules/accessibility';
+import 'highcharts/themes/adaptive';
+import { inject, onMounted, ref } from 'vue';
 
-const chart = ref(null);
+const { isDark } = inject(themingControlKey) as ThemingControl;
+
+Highcharts.setOptions({
+  chart: {
+    backgroundColor: 'transparent',
+    reflow: true,
+  },
+  responsive: {
+    rules: [],
+  },
+  tooltip: {
+    backgroundColor: 'var(--el-text-color-primary)',
+    style: {
+      color: 'var(--el-bg-color)',
+    },
+  },
+});
+
+const chart = ref<Highcharts.Chart | null>(null);
 
 onMounted(() => {
-  if (!chart.value) return;
-
-  Highcharts.chart(chart.value, {
+  chart.value = Highcharts.chart('chart', {
     chart: {
-      type: 'line',
+      type: 'bar',
+      reflow: true,
     },
     title: {
-      text: 'Highcharts ohne Wrapper',
+      text: '',
     },
     xAxis: {
       categories: ['Jan', 'Feb', 'März', 'Apr'],
@@ -21,7 +42,7 @@ onMounted(() => {
       {
         name: 'Beispielreihe',
         data: [1, 3, 2, 4],
-        type: 'line',
+        type: 'bar',
       },
     ],
   });
@@ -29,7 +50,17 @@ onMounted(() => {
 </script>
 
 <template>
-  <div ref="chart"></div>
+  <el-card class="block overflow-hidden">
+    <div class="grid-container">
+      <div class="block" id="chart" :class="isDark ? 'highcharts-dark' : 'highcharts-light'"></div>
+    </div>
+  </el-card>
 </template>
 
-<style scoped></style>
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: auto;
+}
+</style>
