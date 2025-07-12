07aaea8f0f497fbf18b9780c25e4acfd65621cf5
Stefan Jeitler <stefan.jeitler@outlook.com>
2025-07-12 11:30:57 +0200

    test: 'calculateWidgetValues' tests added

---
+++ src/bodyData/aggregations/__tests__/widget-values.test.ts
@@ -0,0 +1,161 @@
+import {
+  type Range,
+  type SingleDay,
+  calculateWidgetValues,
+} from '@/bodyData/aggregations/widget-values';
+import type { BodyData } from '@/bodyData/body-data.types';
+import type { NumberKeys } from '@/types/type-helpers';
+import { endOfMonth, parseISO } from 'date-fns';
+import { describe, expect, test } from 'vitest';
+
+import { getBoundaryRecords } from '../boundaries';
+
+const bodyDataTemplate: BodyData = {
+  recordedAt: parseISO('2025-01-01'),
+  weight: 65,
+  muscleMass: 45,
+  bodyFat: 13,
+  water: 50,
+  bmi: 21,
+  dailyCalorieRequirement: 2100,
+};
+
+function startOfJulyWith(key: NumberKeys<BodyData>, value: number): BodyData {
+  return {
+    ...bodyDataTemplate,
+    recordedAt: parseISO('2025-07-01'),
+    [key]: value,
+  };
+}
+
+function endOfJulyWith(key: NumberKeys<BodyData>, value: number): BodyData {
+  return {
+    ...bodyDataTemplate,
+    recordedAt: endOfMonth(parseISO('2025-07-01')),
+    [key]: value,
+  };
+}
+
+function midAugustWith(key: NumberKeys<BodyData>, value: number): BodyData {
+  return {
+    ...bodyDataTemplate,
+    recordedAt: parseISO('2025-08-16T12:00:00'),
+    [key]: value,
+  };
+}
+
+function endOfAugustWith(key: NumberKeys<BodyData>, value: number): BodyData {
+  return {
+    ...bodyDataTemplate,
+    recordedAt: endOfMonth(parseISO('2025-08-01')),
+    [key]: value,
+  };
+}
+
+function toBoundaryRecords(head: BodyData, ...tail: BodyData[]) {
+  return getBoundaryRecords([head, ...tail]);
+}
+
+describe('calculateWidgetValues', () => {
+  test('returns the correct average monthly weight change', () => {
+    const boundaries = toBoundaryRecords(
+      startOfJulyWith('weight', 66),
+      endOfJulyWith('weight', 65),
+    );
+
+    const widgetValues = calculateWidgetValues('weight', boundaries);
+
+    expect(widgetValues.state).toBe('range');
+    const range = widgetValues as Range;
+    expect(range.averageMonthlyChange).toBeCloseTo(1, 4);
+  });
+
+  test('returns the correct average weekly weight change', () => {
+    const boundaries = toBoundaryRecords(
+      startOfJulyWith('weight', 66),
+      endOfJulyWith('weight', 65),
+    );
+
+    const widgetValues = calculateWidgetValues('weight', boundaries);
+
+    expect(widgetValues.state).toBe('range');
+    const range = widgetValues as Range;
+    expect(range.averageWeeklyChange).toBeCloseTo(0.2258, 4);
+  });
+
+  test('average monthly weight change is equal to change when time range is one month', () => {
+    const boundaries = toBoundaryRecords(
+      startOfJulyWith('weight', 66),
+      endOfJulyWith('weight', 65),
+    );
+
+    const widgetValues = calculateWidgetValues('weight', boundaries);
+
+    expect(widgetValues.state).toBe('range');
+    const range = widgetValues as Range;
+    const changeAbsolute = Math.abs(range.averageMonthlyChange);
+    expect(range.averageMonthlyChange).toBeCloseTo(changeAbsolute, 4);
+  });
+
+  test('returns the correct total wheight change', () => {
+    const boundaries = toBoundaryRecords(
+      startOfJulyWith('weight', 66),
+      endOfJulyWith('weight', 65),
+    );
+
+    const widgetValues = calculateWidgetValues('weight', boundaries);
+
+    expect(widgetValues.state).toBe('range');
+    const range = widgetValues as Range;
+    expect(range.change).toBeCloseTo(-1, 4);
+  });
+
+  test('average monthly weight change is respecting months exactly', () => {
+    const boundaries = toBoundaryRecords(
+      startOfJulyWith('weight', 66),
+      midAugustWith('weight', 67.5),
+    );
+
+    const widgetValues = calculateWidgetValues('weight', boundaries);
+
+    expect(widgetValues.state).toBe('range');
+    const range = widgetValues as Range;
+    expect(range.averageMonthlyChange).toBeCloseTo(1, 4);
+  });
+
+  test('average monthly muscle mass change is respecting months exactly', () => {
+    const boundaries = toBoundaryRecords(
+      startOfJulyWith('muscleMass', 45),
+      midAugustWith('muscleMass', 46),
+    );
+
+    const widgetValues = calculateWidgetValues('muscleMass', boundaries);
+
+    expect(widgetValues.state).toBe('range');
+    const range = widgetValues as Range;
+    expect(range.averageMonthlyChange).toBeCloseTo(0.66666, 4);
+  });
+
+  test('returns empty widget values when boundaries are not present', () => {
+    const widgetValues = calculateWidgetValues('muscleMass', null);
+
+    expect(widgetValues.state).toBe('empty');
+  });
+
+  test('returns sinlge day widget values when there is only one body data record', () => {
+    const boundaries = toBoundaryRecords(endOfAugustWith('weight', 65));
+    const widgetValues = calculateWidgetValues('weight', boundaries);
+
+    expect(widgetValues.state).toBe('singleDay');
+  });
+
+  test('sinlge day widget values are correct', () => {
+    const boundaries = toBoundaryRecords(endOfAugustWith('weight', 65));
+    const widgetValues = calculateWidgetValues('weight', boundaries);
+
+    expect(widgetValues.state).toBe('singleDay');
+    const singleDay = widgetValues as SingleDay;
+    expect(singleDay.value).toBe(65);
+    expect(singleDay.recordedAt).toEqual(boundaries?.first.recordedAt);
+  });
+});
