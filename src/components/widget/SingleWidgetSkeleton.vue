ddc744d82c52a0ee61666c678278e4c9a564a2b5
Stefan Jeitler <stefan.jeitler@outlook.com>
2025-07-18 18:05:11 +0200

    fix: widget skeleton background color fixed

--- src/components/widget/SingleWidgetSkeleton.vue
+++ src/components/widget/SingleWidgetSkeleton.vue
@@ -5,33 +5,39 @@ const props = defineProps<{
 </script>
 
 <template>
-  <el-card>
-    <template #header>
-      <div class="flex flex-row flex-nowrap items-center justify-between">
-        <el-text size="large">{{ $t(props.titleMessageKey) }}</el-text>
-      </div>
-    </template>
-    <template #default>
-      <div class="flex flex-col flex-nowrap items-center justify-start">
-        <div class="w-40">
-          <el-skeleton style="--el-skeleton-circle-size: 130px" animated>
-            <template #template>
-              <div class="flex flex-col flex-nowrap items-center justify-start">
-                <el-skeleton-item variant="circle" />
-                <el-skeleton-item class="skeleton-widget-item mt-4" />
-                <el-skeleton-item class="skeleton-widget-item mt-6" />
-                <el-skeleton-item class="skeleton-widget-item mt-6" />
-              </div>
-            </template>
-          </el-skeleton>
+  <div class="dark:bg-zinc-800">
+    <el-card class="widget-skeleton">
+      <template #header>
+        <div class="flex flex-row flex-nowrap items-center justify-between">
+          <el-text size="large">{{ $t(props.titleMessageKey) }}</el-text>
         </div>
-      </div>
-    </template>
-  </el-card>
+      </template>
+      <template #default>
+        <div class="flex flex-col flex-nowrap items-center justify-start">
+          <div class="w-40">
+            <el-skeleton style="--el-skeleton-circle-size: 130px" animated>
+              <template #template>
+                <div class="flex flex-col flex-nowrap items-center justify-start">
+                  <el-skeleton-item variant="circle" />
+                  <el-skeleton-item class="widget-skeleton-item mt-4" />
+                  <el-skeleton-item class="widget-skeleton-item mt-6" />
+                  <el-skeleton-item class="widget-skeleton-item mt-6" />
+                </div>
+              </template>
+            </el-skeleton>
+          </div>
+        </div>
+      </template>
+    </el-card>
+  </div>
 </template>
 
 <style scoped>
-.skeleton-widget-item {
+.widget-skeleton {
+  background-color: var(--el-mask-color);
+}
+
+.widget-skeleton-item {
   height: 21px;
 }
 </style>
