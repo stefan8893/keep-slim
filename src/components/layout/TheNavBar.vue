8658131be0e89883e5ca9808aecb8270ea7d21db
Stefan Jeitler <stefan.jeitler@outlook.com>
2025-07-19 07:39:36 +0200

    fix: remove user-settings from nav bar

--- src/components/layout/TheNavBar.vue
+++ src/components/layout/TheNavBar.vue
@@ -25,7 +25,6 @@ const activeMenu = computed(() => {
     :ellipsis-icon="ArrowDown"
   >
     <el-menu-item index="body-data">{{ $t(MessageKey.bodyData) }}</el-menu-item>
-    <el-menu-item index="user-settings">{{ $t(MessageKey.userSettings) }}</el-menu-item>
   </el-menu>
 </template>
 
