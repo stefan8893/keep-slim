7787b0a752a326ab6812920cb4ebee50507782e4
Stefan Jeitler <stefan.jeitler@outlook.com>
2025-07-20 12:43:12 +0200

    fix: widget skeleton styling

--- src/components/widget/BodyDataWidgetSkeletons.vue
+++ src/components/widget/BodyDataWidgetSkeletons.vue
@@ -1,25 +1,22 @@
 <script setup lang="ts">
 import SingleWidgetSkeleton from '@/components/widget/SingleWidgetSkeleton.vue';
 import { MessageKey } from '@/i18n/message-keys.g';
+
+const widgets: MessageKey[] = [
+  MessageKey.weight,
+  MessageKey.muscleMass,
+  MessageKey.bodyFat,
+  MessageKey.water,
+];
 </script>
 
 <template>
-  <div class="flex flex-row flex-wrap items-center justify-between gap-y-4">
+  <div class="xs:justify-between flex flex-row flex-wrap items-center justify-center gap-y-4">
     <SingleWidgetSkeleton
-      :title-message-key="MessageKey.weight"
-      class="xs:w-[calc(50%-0.5rem)] w-full md:w-[calc(33.333%-0.666rem)] lg:w-[calc(25%-0.75rem)]"
-    />
-    <SingleWidgetSkeleton
-      :title-message-key="MessageKey.muscleMass"
-      class="xs:w-[calc(50%-0.5rem)] w-full md:w-[calc(33.333%-0.666rem)] lg:w-[calc(25%-0.75rem)]"
-    />
-    <SingleWidgetSkeleton
-      :title-message-key="MessageKey.bodyFat"
-      class="xs:w-[calc(50%-0.5rem)] w-full md:w-[calc(33.333%-0.666rem)] lg:w-[calc(25%-0.75rem)]"
-    />
-    <SingleWidgetSkeleton
-      :title-message-key="MessageKey.water"
-      class="xs:w-[calc(50%-0.5rem)] w-full md:w-[calc(33.333%-0.666rem)] lg:w-[calc(25%-0.75rem)]"
+      v-for="title in widgets"
+      :key="title"
+      :title-message-key="title"
+      class="xs:w-[calc(50%-0.5rem)] w-[calc(100%-2.75rem)] md:w-[calc(33.333%-0.666rem)] lg:w-[calc(25%-0.75rem)]"
     />
   </div>
 </template>
